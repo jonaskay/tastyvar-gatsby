1c9ce8f10a86f036d0f186799d92dc51c86ecd4c
jonaskay <jonaskay@iki.fi>
2020-07-13 20:12:26 +0300

    Add date to article header

--- src/templates/content.js
+++ src/templates/content.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Header from "../components/header"
 
 const Content = ({ data }) => {
-  const { site, content } = data
+  const { content } = data
 
   return (
     <Layout>
@@ -13,9 +13,14 @@ const Content = ({ data }) => {
       <article>
         <header className="circuit-board border-b border-indigo-300">
           <div className="md:max-w-2xl mx-auto bg-indigo-100 md:border-r md:border-l border-indigo-300">
-            <h1 className="max-w-2xl mx-auto pl-8 pr-4 py-4 sm:pl-10 sm:pr-8 sm:py-8 text-2xl sm:text-5xl font-bold">
-              {content.frontmatter.title}
-            </h1>
+            <div className="max-w-2xl mx-auto pl-8 pr-6 py-4 sm:pl-10 sm:pr-8 sm:py-8">
+              <h1 className="mb-4 text-2xl sm:text-5xl font-bold">
+                {content.frontmatter.title}
+              </h1>
+              <div className="text-right">
+                Updated {content.frontmatter.date}
+              </div>
+            </div>
           </div>
         </header>
         <div
@@ -34,6 +39,7 @@ export const query = graphql`
       html
       frontmatter {
         title
+        date(formatString: "MMMM Do YYYY")
       }
     }
   }
