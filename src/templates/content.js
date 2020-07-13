361bfc51d10386dcf26e33c9b7674d49eb0e215a
jonaskay <jonaskay@iki.fi>
2020-07-13 15:55:36 +0300

    Add heading links

--- src/templates/content.js
+++ src/templates/content.js
@@ -9,7 +9,7 @@ const Content = ({ data }) => {
   return (
     <Layout>
       <header className="bg-gray-900 text-gray-100">
-        <div className="max-w-screen-xl mx-auto px-4 sm:px-8 lg:px-16 py-2">
+        <div className="max-w-screen-xl mx-auto pl-8 pr-6 sm:px-10 lg:px-16 py-2">
           <Link to="/" className="brand text-lg font-normal">
             {site.siteMetadata.title}
             <span />
@@ -19,13 +19,13 @@ const Content = ({ data }) => {
       <article>
         <header className="circuit-board border-b border-indigo-300">
           <div className="md:max-w-2xl mx-auto bg-indigo-100 md:border-r md:border-l border-indigo-300">
-            <h1 className="max-w-2xl mx-auto p-4 sm:p-8 text-2xl sm:text-5xl font-bold">
+            <h1 className="max-w-2xl mx-auto pl-8 pr-4 py-4 sm:pl-10 sm:pr-8 sm:py-8 text-2xl sm:text-5xl font-bold">
               {content.frontmatter.title}
             </h1>
           </div>
         </header>
         <div
-          className="markdown p-4 sm:p-8 max-w-2xl mx-auto leading-relaxed"
+          className="markdown pl-8 pr-6 py-4 sm:pl-10 sm:pr-8 sm:py-8 max-w-2xl mx-auto leading-relaxed"
           dangerouslySetInnerHTML={{ __html: content.html }}
         />
       </article>
