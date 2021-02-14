6f8da139b6bf04cbaf8c20a26d93dc89c9fbe9b3
Joonas Kykkänen <jonaskay@iki.fi>
2021-02-14 15:52:56 +0200

    Replace padding with margin

--- src/components/mdx/list.tsx
+++ src/components/mdx/list.tsx
@@ -5,7 +5,7 @@ type ListComponent = React.FC<any> & { Item: React.FC<any> }
 const ListItem: React.FC<any> = props => <li className="my-2" {...props} />
 
 const List: ListComponent = props => (
-  <ul className="list-disc pl-8" {...props} />
+  <ul className="list-disc ml-8" {...props} />
 )
 
 List.Item = ListItem
