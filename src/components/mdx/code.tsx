acdbaa0a90bc8b735a4c80c8591180aea3e69cb2
Joonas Kykkänen <jonaskay@iki.fi>
2021-02-14 15:52:44 +0200

    Fix code component style

--- src/components/mdx/code.tsx
+++ src/components/mdx/code.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Code: React.FC<any> = props => (
   <code
-    className="block overflow-x-auto bg-gray-100 p-4 border rounded"
+    className="block overflow-x-auto bg-gray-100 p-4 border rounded font-mono"
     tabIndex={0}
     {...props}
   />
